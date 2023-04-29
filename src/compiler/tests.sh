aea7efd4926bb6298a573f2b1bdbf843971690a3
Rhishabh Hattarki <rhattark@asu.edu>
2023-04-29 16:30:14 -0700

    updated tests

--- src/compiler/tests.sh
+++ src/compiler/tests.sh
@@ -5,22 +5,22 @@ cwd="$(dirname "$0")"
 echo "Count char"
 "${cwd}/hamlet" "${cwd}/../../data/count_char.hamlet"
 
-# echo $'\n'
-# echo "Example"
-# ./hamlet ../../data/example.hamlet
+echo $'\n'
+echo "Example"
+"${cwd}/hamlet" "${cwd}/../../data/example.hamlet"
 
-# echo $'\n'
-# echo "Factorial"
-# ./hamlet ../../data/factorial.hamlet
+echo $'\n'
+echo "Factorial"
+"${cwd}/hamlet" "${cwd}/../../data/factorial.hamlet"
 
-# echo $'\n'
-# echo "Fibonacci"
-# ./hamlet ../../data/fibonacci.hamlet
+echo $'\n'
+echo "Fibonacci"
+"${cwd}/hamlet" "${cwd}/../../data/fibonacci.hamlet"
 
-# echo $'\n'
-# echo "Hello world"
-# ./hamlet ../../data/hello_world.hamlet
+echo $'\n'
+echo "Hello world"
+"${cwd}/hamlet" "${cwd}/../../data/hello_world.hamlet"
 
-# echo $'\n'
-# echo "Sum of squares"
-# ./hamlet ../../data/sum_of_squares.hamlet
+echo $'\n'
+echo "Sum of squares"
+"${cwd}/hamlet" "${cwd}/../../data/sum_of_squares.hamlet"
