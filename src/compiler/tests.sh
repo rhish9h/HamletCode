cd293ec8c8356aea0fc288b67d18c6e0b0e0b9da
Rhishabh Hattarki <rhattark@asu.edu>
2023-04-29 16:27:33 -0700

    scripts can be run from anywhere now

--- src/compiler/tests.sh
+++ src/compiler/tests.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+cwd="$(dirname "$0")"
+
 echo "Count char"
-./hamlet ../../data/count_char.hamlet
+"${cwd}/hamlet" "${cwd}/../../data/count_char.hamlet"
 
-echo $'\n'
-echo "Example"
-./hamlet ../../data/example.hamlet
+# echo $'\n'
+# echo "Example"
+# ./hamlet ../../data/example.hamlet
 
-echo $'\n'
-echo "Factorial"
-./hamlet ../../data/factorial.hamlet
+# echo $'\n'
+# echo "Factorial"
+# ./hamlet ../../data/factorial.hamlet
 
-echo $'\n'
-echo "Fibonacci"
-./hamlet ../../data/fibonacci.hamlet
+# echo $'\n'
+# echo "Fibonacci"
+# ./hamlet ../../data/fibonacci.hamlet
 
-echo $'\n'
-echo "Hello world"
-./hamlet ../../data/hello_world.hamlet
+# echo $'\n'
+# echo "Hello world"
+# ./hamlet ../../data/hello_world.hamlet
 
-echo $'\n'
-echo "Sum of squares"
-./hamlet ../../data/sum_of_squares.hamlet
+# echo $'\n'
+# echo "Sum of squares"
+# ./hamlet ../../data/sum_of_squares.hamlet
