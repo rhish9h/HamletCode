14e7be717838af4bee3736406bfafff270b41f37
Rhishabh Hattarki <rhattark@asu.edu>
2023-04-29 10:34:56 -0700

    test file added - needs customisation

---
+++ src/runtime/test.pl
@@ -0,0 +1,12 @@
+:- include(evaluator).
+
+test_case(1, [begin, var, x,;, var, y,;, var, z,;, z,:=,x,+,y, end, .], 2, 3, 5).
+
+run(Case) :-
+    test_case(Case, Tokens, X, Y, Expected_Z),
+    write('\nTest case '), write(Case), write(':\n\n'),
+    (current_predicate(program/3), current_predicate(program_eval/4) ->
+        findall((Z), (program(P, Tokens, []), program_eval(P, X, Y, Z)), Ans_Set), forall(member(Ans, Ans_Set), (write(Ans=Expected_Z),nl)) ;
+        write('\n***** Missing either program/3 or program_eval/4 *****\n\n')).
+
+run :- findall((Case), run(Case), _), halt.
