e9162f9960b33d9b85f4a12d01916772f57ce012
Rhishabh Hattarki <rhattark@asu.edu>
2023-04-29 11:17:15 -0700

    added letter_eval

--- src/runtime/test.pl
+++ src/runtime/test.pl
@@ -1,6 +1,10 @@
 :- include(evaluator).
+:- include('../compiler/parse_tree').
 
 test_case(1, [begin, var, x,;, var, y,;, var, z,;, z,:=,x,+,y, end, .], 2, 3, 5).
+
+% Command to run the tests
+% swipl -f .\test.pl -g run
 
 run(Case) :-
     test_case(Case, Tokens, X, Y, Expected_Z),
