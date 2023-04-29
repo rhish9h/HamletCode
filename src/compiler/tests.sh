c6d7dd0d4efd3146e4f117a31ca820681cb1b982
Rhishabh Hattarki <rhattark@asu.edu>
2023-04-29 10:27:56 -0700

    added automated tests

---
+++ src/compiler/tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Count char"
+./hamlet ../../data/count_char.hamlet
+
+echo $'\n'
+echo "Example"
+./hamlet ../../data/example.hamlet
+
+echo $'\n'
+echo "Factorial"
+./hamlet ../../data/factorial.hamlet
+
+echo $'\n'
+echo "Fibonacci"
+./hamlet ../../data/fibonacci.hamlet
+
+echo $'\n'
+echo "Hello world"
+./hamlet ../../data/hello_world.hamlet
+
+echo $'\n'
+echo "Sum of squares"
+./hamlet ../../data/sum_of_squares.hamlet
